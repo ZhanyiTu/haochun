51982e5687b92be9d795501ab1a3c2d7be429b7e
tomtu <125548913@qq.com>
2019-10-29 10:48:54 +0800

    R = [[]] * 9是一种每个数组的索引都相同的数组,是完全一样的9个数组,这里我们要的显然是9个不同的数组 末尾的索引并不是len(R[i])-1而是8 函数逻辑出现了问题 要调整

--- 新建文本文档.py
+++ 新建文本文档.py
@@ -1,40 +1,40 @@
 def update(R, i, t):
     if i == 0:
         if R[i][t] == 0:
-            if R[len(R[i]) - 1][t] == 1:
-                R[i][t+1] = 1
+            if R[8][t] == 1:
+                R[i].append(1)
             else:
-                R[i][t+1] == 0
+                R[i].append(0)
         elif R[i][t] == 1:
-            if R[len(R[i])-1][t] == 0 and R[i+1][t] == 0:
-                R[i][t+1] = 0
+            if R[8][t] == 0 and R[i+1][t] == 0:
+                R[i].append(0)
             else:
-                R[i][t+1] = 1
-    elif i > 0 and i < len(R[i] - 1):
+                R[i].append(1)
+    elif i > 0 and i < 8:
         if R[i][t] == 0:
             if R[i-1][t] == 1:
-                R[i][t+1] = 1
+                R[i].append(1)
             else:
-                R[i][t+1] == 0
+                R[i].append(0)
     
         elif R[i][t] == 1:
             if R[i-1][t] == 0 and R[i+1][t] == 0:
-                R[i][t+1] = 0
+                R[i].append(0)
             else:
-                R[i][t+1] = 1
-    elif i == len(R[i]) - 1:
+                R[i].append(1)
+    elif i == 8:
         if R[i][t] == 0:
             if R[i-1][t] == 1:
-                R[i][t+1] = 1
+                R[i].append(1)
             else:
-                R[i][t+1] == 0
+                R[i].append(0)
     
         elif R[i][t] == 1:
             if R[i-1][t] == 0 and R[0][t] == 0:
-                R[i][t+1] = 0
+                R[i].append(0)
             else:
-                R[i][t+1] = 1
-R = [[]] * 9
+                R[i].append(1)
+R = [[] for i in range(9)]
 R[0].append(1)
 R[1].append(0)
 R[2].append(0)
@@ -45,11 +45,12 @@ R[6].append(0)
 R[7].append(0)
 R[8].append(1)
 for i in range(9):
-    for j in range(len(R[i])):
+    for j in range(1):
         update(R, i, j)
 for i in range(9):
     for j in range(len(R[i])):
-        print(R, i, j)
+        print(R[i][j], end='')
+    print()
 
 '''
     "1 & 0 & 1 & 0\\\\\n",
